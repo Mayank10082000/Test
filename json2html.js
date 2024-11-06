0c066ae758359e00e34128b03777f2ff7c453d18
Mayank Gupta <67310822+Mayank10082000@users.noreply.github.com>
2024-11-06 14:25:30 +0530

    Update json2html.js

--- json2html.js
+++ json2html.js
@@ -1,1 +1,32 @@
+// json2html.js
+export default function json2html(data) {
+    // Get all unique columns from the data
+    const columns = Array.from(new Set(
+        data.reduce((cols, row) => {
+            return cols.concat(Object.keys(row));
+        }, [])
+    ));
 
+    // Build the HTML table string
+    let html = '<table data-user="mayankasheshgupta@gmail.com">\n';
+    
+    // Add header row
+    html += '  <thead>\n    <tr>';
+    columns.forEach(col => {
+        html += `<th>${col}</th>`;
+    });
+    html += '</tr>\n  </thead>\n';
+    
+    // Add body rows
+    html += '  <tbody>\n';
+    data.forEach(row => {
+        html += '    <tr>';
+        columns.forEach(col => {
+            html += `<td>${row[col] || ''}</td>`;
+        });
+        html += '</tr>\n';
+    });
+    html += '  </tbody>\n</table>';
+    
+    return html;
+}
